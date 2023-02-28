4a9560e8a3291560dea7def7a6551515ce8659bf
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2023-02-28 20:56:14 +0530

    examples

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,51 @@ fetchify.init({
   baseURL: "https://jsonplaceholder.typicode.com",
 });
 
+//example with GET
 fetchify
   .GET({
     URL: "/todos",
-    cancelKey: "allTodos",
   })
   .then((data) => {
     console.log(data);
   })
   .catch((error) => console.log(error));
 
+  //example with GET query string params
 fetchify
-  .GET({
-    URL: "/todos/2",
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => console.log(error));
+.GET({
+  URL: "/todos",
+  params:{age:25,country:'LK'}
+})
+.then((data) => {
+  console.log(data);
+})
+.catch((error) => console.log(error));
 
-fetchify.cancel("allTodos");
+//example with POST
+fetchify
+.POST({
+  URL: "/users",
+  data:{name:'NINA',age:25}
+})
+.then((data) => {
+  console.log(data);
+})
+.catch((error) => console.log(error));
+
+//example with POST - File upload
+fetchify
+.POST({
+  URL: "/users",
+  data:{name:'NINA',age:25},
+  files:{
+    file:new File(['text'],'text.txt',{type:'text'})
+  }
+})
+.then((data) => {
+  console.log(data);
+})
+.catch((error) => console.log(error));
+
+
+
