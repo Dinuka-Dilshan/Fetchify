d4656fe75d990469e08e771b7ae099f2b7ec4cde
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2023-03-04 15:48:57 +0530

    types & examples

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,15 @@
 import fetchify from "./Fetchify.js";
 
 fetchify.init({
-  baseURL: "https://jsonplaceholder.typicode.com",
+  baseURL: "https://dummyjson.com",
 });
 
 //example with GET
 fetchify
   .GET({
-    url: "/todos",
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => console.log(error));
-
-//example with GET query string params
-fetchify
-  .GET({
-    url: "/todos",
-    queryStringParams: { age: 25, country: "LK" },
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => console.log(error));
-
-//example with POST
-fetchify
-  .POST({
     url: "/users",
-    data: { name: "NINA", age: 25 },
   })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => console.log(error));
-
-//example with POST - File upload
-fetchify
-  .POST({
-    url: "/users",
-    data: { name: "NINA", age: 25 },
-    files: {
-      file: new File(["text,ejheqehqe"], "text.txt", { type: "text" }),
-    },
-  })
-  .then((data) => {
-    console.log(data);
+  .then((response) => {
+    console.log(response.data);
   })
   .catch((error) => console.log(error));
-
-//example cancel Request
-fetchify
-  .GET({
-    url: "/todos",
-    cancelKey: "cancelKeyName",
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => console.log(error));
-
-fetchify.cancel("cancelKeyName");
--- src/types.ts
+++ src/types.ts
@@ -19,3 +19,20 @@ export type InternalFetchifyRequest = FetchifyRequest & {
   headers?: HeadersInit;
   controllers: Map<string, AbortController>;
 };
+
+export type ResponseBuilder = (
+  response: Response
+) => Promise<FetifySuccessType>;
+
+export type FetifyRejectType = {
+  error: string;
+  errorCode: number;
+  response: Response;
+};
+
+export type FetifySuccessType = {
+  data: any;
+  statusCode: number;
+  statusText: string;
+  response: Response;
+};
